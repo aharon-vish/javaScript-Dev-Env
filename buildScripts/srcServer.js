7acac7487595e29e2a422429b8c3d275436188a1
AharonVishinsky <aharon.vish@gmail.com>
2017-01-21 12:53:17 +0200

    add http and json faker

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -14,18 +14,18 @@ const app = express();
 const compilre = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compilre, {
-    noInfo: true,
-    publicPath: config.output.publicPath
+  noInfo: true,
+  publicPath: config.output.publicPath
 }));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        open('http://localhost:' + port);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
 });
